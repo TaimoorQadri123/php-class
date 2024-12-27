8c157107d5e6c9a81e9fd077ff261fde7291800b
TaimoorQadri123 <dchota374@gmail.com>
2024-12-28 01:15:34 +0500

    done

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<h1>first php page</h1>
+<!-- <h1>first php page</h1> -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <!-- php mein html  -->
+    <!-- $ ye variable declare krne k leye use hota hy php m  -->
+     <?php
+      $atr = 'php work by html embadding';
+      $atr1 = 'php work by php embadding';
+      echo "<h1>$str</h1>";
+     ?>
+     <!-- html m php  -->
+     <h1>
+        <?php echo $str?>
+     </h1>
+
+     <!-- professional tariqa html m php hi hy  -->
+    
+ </body>
+ </html>
